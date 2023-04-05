d8079e6ee646066e22145424f8f197f957773722
Aviroop Jana <aviroopjana@gmail.com>
2023-04-06 00:01:31 +0530

    added PayPal.sol

---
+++ contracts/PayPal.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract PayPal {
+    //Define the owner of the Smart Contract
+
+    //Create Struct and Mapping for request, transaction & name
+
+    //Add a name to the walletAddress
+
+    //Create a Request
+
+    //Pay a Request
+
+    //Get all Requests sent to the User
+
+    //Get all historic transactions user has been apart of
+}
+
