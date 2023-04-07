592f4f61c16fc7c56e759d77ff0d77ff819aa384
Aviroop Jana <aviroopjana@gmail.com>
2023-04-07 23:59:11 +0530

    update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,5 @@
 require("@nomiclabs/hardhat-waffle")
 require("@nomiclabs/hardhat-etherscan");
-
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -10,11 +9,14 @@ module.exports = {
   solidity: "0.8.18",
   networks:{
     mumbai:{
-      url: process.env.POLYGON_MUMBAI,
-      accounts:[process.env.PRIVATE_KEY],
+      url: 'https://polygon-mumbai.g.alchemy.com/v2/gjL1J2_ELhnWg-x87yEP6vfQkKeelTqL',
+      accounts:[process.env.PRIVATE_KEY]
+      // gas: 2100000,
+      // gasPrice: 8000000000,
+      // saveDeployments: true,
     }
   },
   etherscan:{
-    apikey: process.env.API_KEY,
+    apiKey: process.env.API_KEY
   }
 };
